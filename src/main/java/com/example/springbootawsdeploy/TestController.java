56a5dfd9992def1ec105528d0cc75001f553d95a
Rajthetrainer <rajendra22wagh@gmail.com>
2025-12-12 13:34:55 +0530

    Update TestController.java

--- src/main/java/com/example/springbootawsdeploy/TestController.java
+++ src/main/java/com/example/springbootawsdeploy/TestController.java
@@ -9,11 +9,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
     @GetMapping("/data")
     public String getData() {
-        return "Hello IBM from AWS Ecs";
+        return "Final IBM from AWS Ecs";
     }
 
     @GetMapping("/message")
     public String getMessage() {
-        return "Have a good day from AWS Ecs";
+        return "Finally Have a good day from AWS Ecs";
     }
 }
